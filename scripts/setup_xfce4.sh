00fdc5f4c766133704034f768e9857ef7f0feb9c
kassle <kassle@krybrig.org>
2023-12-21 14:17:57 +0700

    lightdm and xfce customization

--- scripts/setup_xfce4.sh
+++ scripts/setup_xfce4.sh
@@ -3,10 +3,13 @@
 mkdir -p $1/etc/skel/.config/menus
 cp /home/kassle/project/debian/config/xfce-applications.menu $1/etc/skel/.config/menus
 
-
 mkdir -p $1/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml
 cp /home/kassle/project/debian/config/xfwm4.xml $1/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/
 cp /home/kassle/project/debian/config/xfce4-notifyd.xml $1/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/
 cp /home/kassle/project/debian/config/xfce4-panel.xml $1/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/
 cp /home/kassle/project/debian/config/xfce4-power-manager.xml $1/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/
 cp /home/kassle/project/debian/config/xsettings.xml $1/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp /home/kassle/project/debian/config/xfce4-desktop.xml $1/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/
+
+mkdir -p $1/etc/skel/.config/xfce4/terminal
+cp /home/kassle/project/debian/config/terminalrc $1/etc/skel/.config/xfce4/terminal
