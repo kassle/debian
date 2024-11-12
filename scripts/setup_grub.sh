2d5d9383483fedab18690462411d6177e025ca7d
erdian <erdian@rayslan.com>
2024-11-13 06:16:46 +0700

    optimize xfce build

--- scripts/setup_grub.sh
+++ scripts/setup_grub.sh
@@ -2,3 +2,6 @@
 
 OUTFILE=$1/etc/default/grub.d/ifname.cfg
 echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX net.ifnames=0 biosdevname=0"' > $OUTFILE
+
+OUTFILE=$1/etc/default/grub.d/terminal.cfg
+echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX fbcon=font:TER16x32"' > $OUTFILE
